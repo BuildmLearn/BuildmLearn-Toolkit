d109e7071058bf8ffd6af7ae0ea57492ccdb14c5
Martin Rotter <rotter.martinos@gmail.com>
2014-04-27 13:31:34 +0200

    Added initial localization files.

---
+++ localization/buildmlearn-toolkit-hi_IN.ts
@@ -0,0 +1,450 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hi_IN">
+<context>
+    <name>FlashcardTemplate</name>
+    <message>
+        <location filename="../src/FlashcardTemplate.cpp" line="282"/>
+        <source>Open File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/FlashcardTemplate.cpp" line="283"/>
+        <source>Images (*.png)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/FlashcardTemplate.cpp" line="622"/>
+        <source>Save File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/FlashcardTemplate.cpp" line="624"/>
+        <source>*.buildmlearn</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FormAbout</name>
+    <message>
+        <location filename="../src/gui/formabout.ui" line="111"/>
+        <source>Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formabout.ui" line="156"/>
+        <source>Changelog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formabout.ui" line="188"/>
+        <source>Changelog page is available only in English language.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formabout.cpp" line="48"/>
+        <source>About %1</source>
+        <extracomment>About toolkit dialog title.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formabout.cpp" line="62"/>
+        <source>Changelog not found.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formabout.cpp" line="67"/>
+        <source>&lt;b&gt;%8&lt;/b&gt;&lt;br&gt;&lt;b&gt;Version:&lt;/b&gt; %1 (build on %2 with CMake %3)&lt;br&gt;&lt;b&gt;Revision:&lt;/b&gt; %4&lt;br&gt;&lt;b&gt;Build date:&lt;/b&gt; %5&lt;br&gt;&lt;b&gt;Qt:&lt;/b&gt; %6 (compiled against %7)&lt;br&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formabout.cpp" line="81"/>
+        <source>&lt;body&gt;%5 is an easy-to-use program that helps users make mobile apps without any knowledge of application development.&lt;br&gt;&lt;br&gt;This software is distributed under the terms of BSD 3-Clause License.&lt;br&gt;&lt;br&gt;Contacts:&lt;ul&gt;&lt;li&gt;&lt;a href=&quot;mailto://%1&quot;&gt;%1&lt;/a&gt; ~email&lt;/li&gt;&lt;li&gt;&lt;a href=&quot;%2&quot;&gt;%2&lt;/a&gt; ~website&lt;/li&gt;&lt;/ul&gt;&lt;br&gt;&lt;br&gt;Copyright (C) 2011-%3 %4&lt;/body&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FormMain</name>
+    <message>
+        <location filename="../src/gui/formmain.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formmain.ui" line="28"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formmain.ui" line="46"/>
+        <source>Check for updates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formmain.ui" line="51"/>
+        <source>&amp;About BuildmLearn Toolkit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formmain.cpp" line="87"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formmain.cpp" line="88"/>
+        <source>&amp;Project</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formmain.cpp" line="90"/>
+        <source>&amp;New Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formmain.cpp" line="91"/>
+        <source>&amp;Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formmain.cpp" line="92"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formmain.cpp" line="93"/>
+        <source>&amp;Exit BuildmLearn Toolkit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formmain.cpp" line="94"/>
+        <source>&amp;Generate Application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formmain.cpp" line="104"/>
+        <source>toolbar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formmain.cpp" line="232"/>
+        <source>Open File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formmain.cpp" line="233"/>
+        <source>*.buildmlearn</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FormUpdate</name>
+    <message>
+        <location filename="../src/gui/formupdate.ui" line="14"/>
+        <source>Check for updates</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.ui" line="25"/>
+        <source>Current release</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.ui" line="39"/>
+        <source>Available release</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.ui" line="46"/>
+        <source>Changes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.ui" line="92"/>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="55"/>
+        <source>Update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="56"/>
+        <source>Download new installation files.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="89"/>
+        <source>unknown</source>
+        <extracomment>Unknown release.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="92"/>
+        <source>Error: &apos;%1&apos;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="93"/>
+        <source>List with updates was not
+downloaded successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="96"/>
+        <source>Checking for updates failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="104"/>
+        <source>New release available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="105"/>
+        <source>This is new version which can be
+downloaded and installed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="108"/>
+        <source>Download installation file for your OS.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="109"/>
+        <source>Installation file is not available directly.
+Go to application website to obtain it manually.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="113"/>
+        <source>Download update</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="118"/>
+        <source>No new release available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="119"/>
+        <source>This release is not newer than
+currently installed one.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="121"/>
+        <source>No new update available.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="129"/>
+        <source>Downloaded %1% (update size is %2 kB).</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="135"/>
+        <source>Downloading update...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="183"/>
+        <source>Downloaded successfully, stored in file &apos;%1&apos;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="184"/>
+        <source>Package was downloaded successfully.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="185"/>
+        <source>Open update file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="191"/>
+        <source>Error occured.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="192"/>
+        <source>Error occured during downloading of the package.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="193"/>
+        <source>Error occured</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="211"/>
+        <location filename="../src/gui/formupdate.cpp" line="219"/>
+        <source>Cannot update application</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/formupdate.cpp" line="212"/>
+        <location filename="../src/gui/formupdate.cpp" line="220"/>
+        <source>Cannot navigate to installation file. Check new installation downloads manually on project website.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InfoTemplate</name>
+    <message>
+        <location filename="../src/InfoTemplate.cpp" line="274"/>
+        <source>Save File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/InfoTemplate.cpp" line="276"/>
+        <source>*.buildmlearn</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NetworkFactory</name>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="53"/>
+        <source>protocol error</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="57"/>
+        <source>host not found</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="62"/>
+        <source>connection refused</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="67"/>
+        <source>connection timed out</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="71"/>
+        <source>SSL handshake failed</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="76"/>
+        <source>proxy server connection refused</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="80"/>
+        <source>temporary failure</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="84"/>
+        <source>authentication failed</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="88"/>
+        <source>proxy authentication required</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="92"/>
+        <source>proxy server not found</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="96"/>
+        <source>success</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="100"/>
+        <source>uknown content</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="104"/>
+        <source>content not found</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/network-web/networkfactory.cpp" line="108"/>
+        <source>unknown error</source>
+        <extracomment>Network status.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="56"/>
+        <source>LANG_NAME</source>
+        <extracomment>Name of language, e.g. English.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="60"/>
+        <source>LANG_ABBREV</source>
+        <extracomment>Abbreviation of language. Use ISO 639-1 code here combined with ISO 3166-1 (alpha-2) code. Examples: &quot;cs_CZ&quot;, &quot;en_GB&quot;, &quot;en_US&quot;.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="62"/>
+        <source>LANG_AUTHOR</source>
+        <extracomment>Name of translator - optional.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/main.cpp" line="64"/>
+        <source>LANG_EMAIL</source>
+        <extracomment>Email of translator - optional.</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QuizTemplate</name>
+    <message>
+        <location filename="../src/QuizTemplate.cpp" line="577"/>
+        <source>Save File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/QuizTemplate.cpp" line="579"/>
+        <source>*.buildmlearn</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SystemTrayIcon</name>
+    <message>
+        <location filename="../src/gui/systemtrayicon.cpp" line="107"/>
+        <source>Close opened modal dialogs first.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TrayIconMenu</name>
+    <message>
+        <location filename="../src/gui/systemtrayicon.cpp" line="53"/>
+        <source>Close opened modal dialogs first.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
