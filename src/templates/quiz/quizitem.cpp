9bb94c206191ba9036082c0833fd5ee995ee0129
Martin Rotter <rotter.martinos@gmail.com>
2014-06-24 17:06:50 +0200

    Work on Quiz simulator.

--- src/templates/quiz/quizitem.cpp
+++ src/templates/quiz/quizitem.cpp
@@ -1,8 +1,18 @@
 #include "templates/quiz/quizitem.h"
 
 
-QuizItem::QuizItem(QWidget *parent) : QWidget(parent), m_ui(new Ui::QuizItem) {
+QuizItem::QuizItem(QWidget *parent) : QWidget(parent), m_ui(new Ui::QuizItem), m_state(Unanswered) {
   m_ui->setupUi(this);
+
+  setupButtons();
+  createConnections();
+}
+
+QuizItem::~QuizItem() {
+  delete m_ui;
+}
+
+void QuizItem::setupButtons() {
   m_answerButtons = QList<QRadioButton*>();
   m_answerButtons.append(m_ui->m_rbAnswerOne);
   m_answerButtons.append(m_ui->m_rbAnswerTwo);
@@ -10,8 +20,19 @@ QuizItem::QuizItem(QWidget *parent) : QWidget(parent), m_ui(new Ui::QuizItem) {
   m_answerButtons.append(m_ui->m_rbAnswerFour);
 }
 
-QuizItem::~QuizItem() {
-  delete m_ui;
+void QuizItem::createConnections() {
+  connect(m_ui->m_btnNext, SIGNAL(clicked()), this, SLOT(onNextClicked()));
+  connect(m_ui->m_btnConfirm, SIGNAL(clicked()), this, SLOT(onSubmitClicked()));
+  connect(m_ui->m_rbAnswerOne, SIGNAL(toggled(bool)), m_ui->m_lblWarning, SLOT(hide()));
+  connect(m_ui->m_rbAnswerTwo, SIGNAL(toggled(bool)), m_ui->m_lblWarning, SLOT(hide()));
+  connect(m_ui->m_rbAnswerThree, SIGNAL(toggled(bool)), m_ui->m_lblWarning, SLOT(hide()));
+  connect(m_ui->m_rbAnswerFour, SIGNAL(toggled(bool)), m_ui->m_lblWarning, SLOT(hide()));
+}
+
+void QuizItem::clearStylesheets() {
+  foreach (QRadioButton *answer_button, m_answerButtons) {
+    answer_button->setStyleSheet(QString());
+  }
 }
 
 void QuizItem::setQuestion(const QuizQuestion &question, int question_number) {
@@ -27,11 +48,46 @@ void QuizItem::setQuestion(const QuizQuestion &question, int question_number) {
   m_ui->m_rbAnswerThree->setVisible(question.correctAnswer() == 3 || !question.answerThree().simplified().isEmpty());
   m_ui->m_rbAnswerFour->setVisible(question.correctAnswer() == 4 || !question.answerFour().simplified().isEmpty());
 
-  m_ui->m_rbAnswerOne->setChecked(question.correctAnswer() == 1);
-  m_ui->m_rbAnswerTwo->setChecked(question.correctAnswer() == 2);
-  m_ui->m_rbAnswerThree->setChecked(question.correctAnswer() == 3);
-  m_ui->m_rbAnswerFour->setChecked(question.correctAnswer() == 4);
-
   m_ui->m_lblQuestionNumber->setText(tr("Question number %1").arg(question_number));
   m_ui->m_lblQuestionText->setText(question.question());
+}
+
+QuizItem::State QuizItem::state() const {
+  return m_state;
+}
+
+void QuizItem::onNextClicked() {
+  // Just signal that user is done with this question.
+  emit questionSubmitted();
+}
+
+void QuizItem::onSubmitClicked() {
+  // Check if user selected any answer, if he did not, then remind him it.
+  if (!m_ui->m_rbAnswerOne->isChecked() && !m_ui->m_rbAnswerTwo->isChecked() &&
+      !m_ui->m_rbAnswerThree->isChecked() && !m_ui->m_rbAnswerFour->isChecked()) {
+    // No answer seems to be selected.
+    m_ui->m_lblWarning->setText("Select some answer, please.");
+    m_ui->m_lblWarning->setVisible(true);
+  }
+  else {
+    int selected_answer = 0;
+
+    // User selected some answer, highlight correct and incorrect answer.
+    for (int i = 0; i < m_answerButtons.size(); i++) {
+      if (m_answerButtons.at(i)->isChecked()) {
+        selected_answer = i + 1;
+      }
+    }
+
+    if (selected_answer == m_question.correctAnswer()) {
+      m_answerButtons.at(selected_answer - 1)->setStyleSheet("background-color: green;");
+    }
+    else {
+      m_answerButtons.at(selected_answer - 1)->setStyleSheet("background-color: red;");
+      m_answerButtons.at(m_question.correctAnswer() - 1)->setStyleSheet("background-color: green;");
+    }
+
+    // Move to next question.
+    onNextClicked();
+  }
 }
--- src/templates/quiz/quizitem.h
+++ src/templates/quiz/quizitem.h
@@ -16,13 +16,41 @@ class QuizItem : public QWidget {
     Q_OBJECT
 
   public:
+    enum State {
+      Unanswered,
+      AnsweredCorrectly,
+      AnsweredWrongly
+    };
+
     // Contructors and destructors.
     explicit QuizItem(QWidget *parent = 0);
     virtual ~QuizItem();
 
+    /// \brief Sets new question for this widget.
+    /// \param question Question object.
+    /// \param question_number Number of the question.
     void setQuestion(const QuizQuestion &question, int question_number);
 
+    ///
+    /// \brief Access to state of quiz question widget.
+    /// \return Returns the state of quiz question widget.
+    State state() const;
+
+  signals:
+    /// \brief Emitted if users clicks "Next" or "Submit"
+    void questionSubmitted();
+
+  private slots:
+    void onNextClicked();
+    void onSubmitClicked();
+
   private:
+    void setupButtons();
+    void createConnections();
+    void clearStylesheets();
+
+  private:
+    State m_state;
     Ui::QuizItem *m_ui;
     QuizQuestion m_question;
     QList<QRadioButton*> m_answerButtons;
