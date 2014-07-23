a74c0f9e5b82e900439ef261a7856a2c30b5d9d4
Martin Rotter <rotter.martinos@gmail.com>
2014-07-23 09:37:20 +0200

    Added first version of help contents.

--- src/gui/formhelp.cpp
+++ src/gui/formhelp.cpp
@@ -51,44 +51,46 @@ FormHelp::FormHelp(bool do_not_show_again_enabled, QWidget *parent)
   connect(this, SIGNAL(accepted()), this, SLOT(onAccepted()));
 
   //: Help HTML contents, translate including HTML markup!!!
-  QString help_contents = tr("");
+  QString help_contents = tr("<body>"
+                             "<h2 style=\"text-align: center\">Welcome to help</h2>"
+                             ""
+                             "<p style=\"text-align: center\">"
+                             "<a href=\"#introduction\">Introduction</a> | "
+                             "<a href=\"#simulator\">Simulator</a> | "
+                             "<a href=\"#xml-bundles\">XML bundles</a> | "
+                             "<a href=\"#other-facts\">Other facts</a>"
+                             "</p>"
+                             ""
+                             "<a name=\"introduction\"></a><h3 style=\"text-align: center\">Introduction</h3>"
+                             "<p>"
+                             "BuildmLearn Toolkit is an application which helps you to create interactive mobile applications in a WYSIWYG way. You do not need to know anything about programming, no extra skills are required. You simply put in the content you would like to have in your application and that's all."
+                             "</p>"
+                             ""
+                             "<p>"
+                             "You can start new mobile application project by going into main menu and using item \"Project -> New project\". Toolkit contains several templates to start with, including template for quiz-like applications or template for learning of word spelling. After creating new project, you will see that main window of BuildmLearn Toolkit contains user interface which expects your input, this is the \"editor\" of your project. When you save your project using \"Project -> Save project as...\" menu item, then all data from \"editor\" are saved, so that you can continue in your work later."
+                             "</p>"
+                             ""
+                             "<p>"
+                             "BuildmLearn Toolkit's main ability is that it is able to generate mobile applications for Android (*.apk files) for you. You just need to select template, fill it with data and use menu item \"Project -> Generate mobile application\". Note that this option is only available if \"editor\" of your template contains enough data and application has correct settings. You can tweak relevant settings in \"Tools -> Settings\" dialog, in \"Project .apk file generation\" section. You need to have JRE (Java Runtime Environment) installed in order for this feature to work."
+                             "</p>"
+                             ""
+                             "<a name=\"simulator\"></a><h3 style=\"text-align: center\">Simulator</h3>"
+                             "<p>"
+                             "This applications provides you with option to test you application before you generate it. Simply load your project, fill it with data and hit \"Simulator -> Run new simulation\" menu item. Separate simulator window with your simulation will appear if not visible already."
+                             "</p>"
+                             ""
+                             "<a name=\"xml-bundles\"></a><h3 style=\"text-align: center\">XML bundles</h3>"
+                             "<p>"
+                             "You can save your work so that you can carry it to another computer and/or continue in your work later. You can do that by pushing \"Project -> Save project as...\" menu item. Projects are stored in special XML files with \"*.buildmlearn\" suffix."
+                             "</p>"
+                             ""
+                             "<a name=\"other-facts\"></a><h3 style=\"text-align: center\">Other facts</h3>"
+                             "<p>"
+                             "BuildmLearn Toolkit is portable application which does not story anything to system registry. The only system folders it uses are user-specific folders."
+                             "</p>"
+                             "</body>");
 
-  /*
-<body>
-<h2 style="text-align: center">Welcome to help</h2>
-
-<p style="text-align: center">
-<a href="#introduction">Introduction</a> |
-<a href="#simulator">Simulator</a> |
-<a href="#xml-bundles">XML bundles</a>
-</p>
-
-
-<a name="introduction"></a><h3 style="text-align: center">Introduction</h3>
-<p>
-BuildmLearn Toolkit is an application which helps you to create interactive mobile applications in a WYSIWYG way. You do not need to know anything about programming, no extra skills are required. You simply put in the content you would like to have in your application and that's all.
-</p>
-
-<p>
-You can start new mobile application project by going into main menu and using item "Project -> New project". Toolkit contains several templates to start with, including template for quiz-like applications or template for learning of word spelling. After creating new project, you will see that main window of BuildmLearn Toolkit contains user interface which expects your input, this is the "editor" of your project. When you save your project using "Project -> Save project as..." menu item, then all data from "editor" are saved, so that you can continue in your work later.
-</p>
-
-<p>
-
-</p>
-
-<a name="simulator"></a><h3 style="text-align: center">Simulator</h3>
-<p>
-
-</p>
-
-<a name="xml-bundles"></a><h3 style="text-align: center">XML bundles</h3>
-<p>
-csacas
-</p>
-
-</body>
-*/
+  m_ui->m_txtHelp->setText(help_contents);
 }
 
 FormHelp::~FormHelp() {
