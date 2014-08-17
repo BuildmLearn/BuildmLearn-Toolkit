56d559a82749d5c2102193f6f5a8e18d91d25a7c
Martin Rotter <rotter.martinos@gmail.com>
2014-08-17 19:44:09 +0200

    New doxygen main page docs content.

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@
 #include <QTimer>
 
 
-/// \mainpage Welcome to documentation!!!
+/// \mainpage Welcome to documentation!
 /// The BuildmLearn Toolkit is an easy-to-use
 /// program that helps users make mobile apps
 /// without any knowledge of application development.
@@ -54,6 +54,63 @@
 /// various functionality and allows teachers to input
 /// their custom content. Targeted at teachers, this program
 /// helps them make learning fun and engaging through mobile apps.
+///
+///
+/// Links
+/// -----
+/// * [www.buildmlearn.org](http://buildmlearn.org/)  ~homepage,
+/// * [www.buildmlearn.github.io/BuildmLearn-Toolkit/docs/](http://buildmlearn.github.io/BuildmLearn-Toolkit/docs/)  ~API documentation,
+/// * [www.martin-rotter.8u.cz](http://www.martin-rotter.8u.cz/)  ~author's homepage,
+/// * [www.transifex.com/projects/p/buildmlearn-toolkit](https://www.transifex.com/projects/p/buildmlearn-toolkit/)  ~translations,
+/// * [www.build.opensuse.org/project/show/home:skunkos:buildmlearn] (https://build.opensuse.org/project/show/home:skunkos:buildmlearn)  ~OBS homepage,
+/// * [www.cmake.org](http://www.cmake.org/)  ~build system,
+/// * [www.qt-project.org](http://qt-project.org/)  ~Qt libraries.
+///
+/// Packages
+/// --------
+/// You can use source code tarball available direct from here to build the software. Precompiled binary packages are available for some other platforms:
+///
+/// * [OBS (Git version)](http://software.opensuse.org/download.html?project=home%3Askunkos%3Abuildmlearn&package=buildmlearn-toolkit-git)  ~Archlinux, openSUSE, Fedora, *Ubuntu, Debian,
+/// * [OBS (Stable version)](http://software.opensuse.org/download.html?project=home%3Askunkos%3Abuildmlearn&package=buildmlearn-toolkit)  ~Archlinux, openSUSE, Fedora, *Ubuntu, Debian,
+/// * [testing NSIS installers](https://drive.google.com/folderview?id=0B8XNkQ-jUoBYYjMzd3laRHROMnM&usp=drive_web)  ~Windows (XP or newer),
+/// * TBA  ~OS/2 (eComStation),
+/// * TBA  ~OSX (Mac OS X).
+///
+/// How to build
+/// ------------
+/// Information regarding build process can be found in CMakeLists.txt file.
+///
+/// License for use and distribution
+/// ---------------------------------
+/// All the code in this repository (unless specified otherwise) is governed by the BSD (3-Clause) License quoted below.
+///
+/// Copyright (c) 2012, BuildmLearn Contributors listed at http://buildmlearn.org/people/
+/// All rights reserved.
+///
+/// Redistribution and use in source and binary forms, with or without
+/// modification, are permitted provided that the following conditions are met:
+///
+/// * Redistributions of source code must retain the above copyright notice, this
+///   list of conditions and the following disclaimer.
+///
+/// * Redistributions in binary form must reproduce the above copyright notice,
+///   this list of conditions and the following disclaimer in the documentation
+///   and/or other materials provided with the distribution.
+///
+/// * Neither the name of the BuildmLearn nor the names of its
+///   contributors may be used to endorse or promote products derived from
+///   this software without specific prior written permission.
+///
+/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+/// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+/// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+/// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+/// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+/// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+/// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+/// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+/// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+/// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /// \defgroup template-interfaces Template interfaces
 /// \brief Base template interface classes.
