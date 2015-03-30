f005095fb813ae7ac4988b90e51cdb25721049b8
yogeshmadaan100 <yogeshmadaan100@gmail.com>
2015-03-30 11:47:21 +0530

    Code Clean

--- build/CMakeFiles/lupdate.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "buildmlearn-toolkit_automoc.cpp"
-  "CMakeFiles/lupdate"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/lupdate.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
